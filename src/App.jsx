d7cf1a4b6f9c82ebc7e274f21a198c6d9b6147fd
rong5026 <rong5026@naver.com>
2023-02-17 00:28:06 +0900

    [UPDATE] 웹 채팅 완성

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,35 @@ const socket = io.connect("http://localhost:3000");
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
-      socket.emit("join_room" , room);
+      socket.emit("join_room", room);
+      setShowChat(true);
     }
   };
   return (
     <div className="App">
-      <h3>Join A Chat</h3>
-      <input type="text" placeholder='Name..' onChange={(event) => {
-        setUsername(event.target.value);
-      }}></input>
+      {!showChat ?  (
+      <div className='joinChatContainer'>
+        <h3>Join A Chat</h3>
 
-      <input type="text" placeholder='Room Id..' onChange={(event) => {
-        setRoom(event.target.value);
-      }}></input>
+        <input type="text" placeholder='Name..' onChange={(event) => {
+          setUsername(event.target.value);
+        }}></input>
 
-      <button onClick={joinRoom}>Join</button>
+        <input type="text" placeholder='Room Id..' onChange={(event) => {
+          setRoom(event.target.value);
+        }}></input>
 
+        <button onClick={joinRoom}>Join</button>
+      </div>
+      ): (
       <Chat socket={socket} username={username} room={room}></Chat>
+      )}
     </div>
-  )
+  );
 }
 
 export default App
