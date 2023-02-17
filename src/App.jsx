185bd63ebfa53d24c63a9953d95941001389c231
rong5026 <rong5026@naver.com>
2023-02-17 19:59:11 +0900

    [ADD] /test 채팅페이지

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home } from "./Pages/Home";
+import Lobby from "./Lobby/Lobby.jsx"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/test" element={<Lobby/>} />
     </Routes>
   );
 }
